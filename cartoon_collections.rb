dff9f8b965c356d148bab115aa9c2d348155803f
Jose Chavira <joeychavira2016@gmail.com>
2019-10-21 22:53:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,13 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
+    planeteer_calls.collect do |call|
     call.capitalize + "!"
   end
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |call|
+    planeteer_calls.any? do |call|
     call.length > 4
   end
 end
